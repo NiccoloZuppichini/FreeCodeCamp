35a22bc110d1663e76c9fe96a0e0382332a99355
NiccoloZuppichini <zuppin@usi.ch>
2018-07-01 11:46:58 +0200

    added random img next to quote

--- random_quotes/index.js
+++ random_quotes/index.js
@@ -1,18 +1,40 @@
 $(document).ready(function(){
 
+//get first random img
+get_img();
+
 var quotes = [];
 
-function initQuotes(){
 
+function get_img(keyword){
+  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+{
+  tags: keyword,
+  tagmode: "any",
+  format: "json"
+},
+(data)=>{
+  var random = Math.floor(Math.random() * data.items.length);
+  var img = data.items[random]['media']['m'].replace("_m", "_b");
+  $("#chuck-img").attr("src", img)
+}
+)
 }
 
+
 function getRandomQuote(){
-        $.getJSON("http://api.icndb.com/jokes/random?exclude=[explicit]", 
-        function(json) { 
+        $.getJSON("http://api.icndb.com/jokes/random?exclude=[explicit]",
+        function(json) {
           $("#text").html((json.value.joke));
 });
 }
 
 
-$("#btn").on("click", getRandomQuote)
+
+
+
+$("#btn").on("click", ()=>{
+  get_img('chuck norris');
+  getRandomQuote();
+})
 });
